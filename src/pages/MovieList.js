0306543d80afe8df0aaa08e12f4c0355906c7410
DawidDabrowskii <dabrowskidawid680@gmail.com>
2023-03-10 12:17:08 +0100

    Update
    - Fixing some UI in header issues,
    - Adding custom fetch hook,
    - Adding API calls for each section,
    - Adding 'cards' about movies and filling them with API informations

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,18 @@
-export const MovieList = () => {
+import { Card } from '../components/Card';
+import { useFetch } from '../hooks/useFetch';
+
+export const MovieList = ({ apiPath }) => {
+  const { data: movies } = useFetch(apiPath);
+
   return (
     <main>
-      <div className=''>MovieList</div>
+      <section className='max-w-7xl mx-auto py-7'>
+        <div className='flex justify-start flex-wrap'>
+          {movies.map(movie => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 };
